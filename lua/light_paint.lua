5dc2827f520cb0ab36934fede335849de78b3001
Antoine <antoine@crafting-labs.fr>
2016-07-11 16:35:34 +0200

    light_paint is the module that manage the stick
    - light_paint is a lua module (use with require)
    - add a 5s start delay in the init.lua before calling main

---
+++ lua/light_paint.lua
@@ -0,0 +1,82 @@
+local apawrite = apa102.write
+local file_readline, file_open, file_close = file.readline, file.open, file.close
+local fromHex = encoder.fromHex
+local string_gsub, string_char, string_rep, string_len = string.gsub, string.char, string.rep, string.len
+local tmr = tmr
+
+local print = print 
+local gpio_read, gpio_trig, gpio_mode, gpio_OUTPUT = gpio.read, gpio.trig, gpio.mode, gpio.OUTPUT
+
+local M = {}
+if setfenv then
+    setfenv(1, M) -- for 5.1
+else
+    _ENV = M -- for 5.2
+end
+
+local data = 6
+local clock = 7
+
+local alarm = 0
+
+
+local function off()
+    leds_abgr = string_rep(string_char(0, 0, 0, 0), 112)
+    apawrite(data, clock, leds_abgr)
+end
+
+local function read_line()       
+    line = file_readline()
+    if(line ~= nil and string_len(line) == 1024)
+    then
+        line = line..file_readline()
+    end
+    return line
+end
+
+local function print_line(line)
+    hex = fromHex(line)
+    apawrite(data, clock, hex)
+end
+
+local function print_frame()
+    line = read_line()
+    if (line ~= nil)
+    then
+        line = string_gsub(line, "\n", "")    
+        print_line(line)
+     else
+        file_close()
+        stop()
+     end
+end
+
+function init(data_pin, clock_pin, alarm_id)
+    data = data_pin
+    clock = clock_pin
+    alarm = alarm_id
+    gpio_mode(data, gpio_OUTPUT) 
+    gpio_mode(clock, gpio_OUTPUT)
+
+    leds_abgr = string_rep(string_char(1, 255, 0, 0), 112)
+    apawrite(data, clock, leds_abgr)
+    off()
+end
+
+function print_file(filename, delay)
+    off()
+    ret = file_open(filename, "r")
+    if(not ret)
+    then
+        error("can't open file named: "..filename)
+    end
+    tmr.alarm(alarm, delay, tmr.ALARM_AUTO, print_frame)
+    current = 1
+end
+
+function stop()
+    tmr.stop(alarm)
+    off()
+end
+
+return M
